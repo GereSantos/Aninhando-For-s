537de8662c5212d5f525ec37b7e4512495f72495
Geremias Santos <86251640+GereSantos@users.noreply.github.com>
2022-08-18 15:15:40 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function multiplyAll(arr) {
     let product = 1;
-    // Altere apenas o código abaixo desta linha
+   
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
     product =  product * arr[i][j];
     }
   }
-    // Altere apenas o código acima desta linha
+   
     return product;
   }
   
